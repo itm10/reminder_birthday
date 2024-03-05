2312625f09c6d8ebfbbd058b366a4084649b0568
itm10 <ibrohim.islom10@hmail.com>
2024-03-05 21:07:23 +0500

    Changes finished

--- databases/databases.py
+++ databases/databases.py
@@ -1,13 +1,17 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def connection():
     return psycopg2.connect(
-        dbname='project_db',
-        user='postgres',
-        password='1004200310',
-        host='localhost',
-        port=5432
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
 
 
